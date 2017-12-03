db87554501455c02c94e056fbc33810307217df8
sofiiaAhafonova <amita051197@gmail.com>
2017-12-03 17:11:09 +0200

    api pages

---
+++ routes/api.js
@@ -0,0 +1,8 @@
+let express = require("express");
+let router = express.Router();
+const Project = require('../models/Project')
+const User = require('../models/User');
+var auth = require("../app");
+
+
+module.exports = router;
