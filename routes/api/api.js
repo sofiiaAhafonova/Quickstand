b0cf6a975a0bf9029062068b6034d9da6f74a057
sofiiaAhafonova <amita051197@gmail.com>
2017-12-04 22:43:37 +0200

    api  proj

--- routes/api/api.js
+++ routes/api/api.js
@@ -6,6 +6,9 @@ var auth = require("./auth");
 const projects = require('./projects')
 const users = require('./users')
 
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
 router.use('/projects', auth.authCheck, projects);
 router.use('/users',auth.authCheck, auth.adminCheck, users)
 
