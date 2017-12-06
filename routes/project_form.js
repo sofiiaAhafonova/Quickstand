b08f2009faedfe537f7fab4e3360947592648771
sofiiaAhafonova <amita051197@gmail.com>
2017-12-07 00:22:32 +0200

    upload images to cloud

--- routes/project_form.js
+++ routes/project_form.js
@@ -4,6 +4,7 @@ const User = require('../models/User');
 const fs = require("fs-promise");
 var Project = require('../models/Project');
 var cloudinary = require('cloudinary');
+let CLOUDINARY_URL = "cloudinary://365483611972472:WDcwHCjlZvJWHdDaFr9fjRRNv-k@de46jchnd"
 
 router.get("/", (req, res, next) => {
     try {
@@ -16,27 +17,36 @@ router.get("/", (req, res, next) => {
         res.sendStatus(500);
     }
 });
+cloudinary.config({
+    cloud_name: 'de46jchnd',
+    api_key: '365483611972472',
+    api_secret: 'WDcwHCjlZvJWHdDaFr9fjRRNv-k'
 
+});
 router.post("/", function (req, res, next) {
     if (!req.body) {
         console.log(req.body)
         return res.sendStatus(400);
     }
     let proj = req.body;
-    proj.user =  req.user._id;
-    proj.image = req.files.logo.data
-    Project.create({
-               proj
-            },
+    proj.user = req.user._id;
+    let name_img =  proj.name
+    cloudinary.uploader.upload_stream(function (result) {
+        console.log(result);
+    }, {
+        public_id:name_img
+    }).end((req.files.logo.data));
+    proj.image = "http://res.cloudinary.com/de46jchnd/image/upload/v1512598615/" + proj.name + ".jpg";
+    Project.create(
+            proj,
             function (err, doc) {
-                if (err)
-                {
+                if (err) {
                     console.log(err)
                     res.redirect('/project_form');
-                 
+
                     return;
                 }
-                   
+
                 console.log("added");
                 req.user.projects.push(doc._id);
                 req.user.save();
