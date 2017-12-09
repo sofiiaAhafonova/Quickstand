efc824c4d8eaec5d91b24d9e7e6bf2d9ad73d463
sofiiaAhafonova <amita051197@gmail.com>
2017-12-09 03:30:48 +0200

    projects

--- public/javascripts/searchProject.js
+++ public/javascripts/searchProject.js
@@ -1,29 +1,34 @@
 
 function updateResults(page, value) {
-	var str = '';
-	if (value && page)
-		str += "?name=" + value + "&page=" + page;
-	else if (page)
-		str += "?page=" + page;
-	else if (value)
-		str += "?search=" + value;
-	console.log(str);
 	var url = 'http://localhost:8080/api/v1/projects/';
-	Handlebars.registerHelper("for", function (number) {
-		var src = '';
-		for (var i = 1; i <= number; i++) {
-			src += '<li class="page-item">' +
-                ' <button class="page-link"  onclick="updateResults(' + i + ',' + value + ')" >'
-                 + i + '</button></li>';
-		}
-		return src;
-    });
+	// Handlebars.registerHelper("for", function (number) {
+	// 	var src = '';
+	// 	for (var i = 1; i <= number; i++) {
+	// 		src += '<li class="page-item">' +
+    //             ' <button class="page-link"  onclick="updateResults(' + i + ',' + value + ')" >'
+    //              + i + '</button></li>';
+	// 	}
+	// 	return src;
+    // });
 	 fetch(url, {
         method: 'get',
         credentials: 'include',
 		headers: {
 			Authorization: "Basic " + Cookie('basic')
 		}
-     }).then(data => data.json()).then(res=> console.log(res) )
+     }).then(data => data.json()).then(res=> {
+         console.log(res)
+         const source = document.getElementById("projects-template").innerHTML;
+         const template = Handlebars.compile(source);
+       
+    
+         console.log(  template({projects: res["projects"]}));
+         document.getElementById("projects-list").innerHTML = template({projects: res["projects"]});
+         
+        })
+
+}
+
+function  header() {
 
 }
